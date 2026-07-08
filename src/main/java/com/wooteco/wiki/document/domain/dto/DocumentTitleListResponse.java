dcad2ac5411508015cfc460f741095e6d62e47a5
jyt6640 <jyt6640@gmail.com>
2026-07-08 13:44:54 +0900

    feat: DocumentTitleListResponse에 DocumentType, generateTime 필드 추가

--- src/main/java/com/wooteco/wiki/document/domain/dto/DocumentTitleListResponse.java
+++ src/main/java/com/wooteco/wiki/document/domain/dto/DocumentTitleListResponse.java
@@ -1,9 +1,22 @@
 package com.wooteco.wiki.document.domain.dto;
 
+import com.wooteco.wiki.document.domain.DocumentType;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record DocumentTitleListResponse(
         String title,
-        UUID uuid
+        UUID uuid,
+        DocumentType documentType,
+        LocalDateTime generateTime
 ) {
+
+    public DocumentTitleListResponse(
+            String title,
+            UUID uuid,
+            String documentType,
+            LocalDateTime generateTime
+    ) {
+        this(title, uuid, DocumentType.valueOf(documentType), generateTime);
+    }
 }
